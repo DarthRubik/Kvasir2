c9ab562119753852d9ebef645e5a2c24fe70185f
Jacob Gamble <gamblejr@prototypeInd.com>
2019-06-25 18:12:26 -0500

    Getting rid of extra tabs that got in there somehow

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ int main(void)
 {
     using loc1 = bit_location<100,1<<0,bool,0>;
     using loc2 = bit_location<200,1<<5,bool,5>;
-	
-	debug_memory[200] = 0xffffffff;
-	
+
+    debug_memory[200] = 0xffffffff;
+
     bool x = std::get<0>(
         apply(
             set_value<loc1,true>{},
@@ -18,7 +18,7 @@ int main(void)
             read_value<loc1>{}
         )
     );
-	assert(debug_memory[100] == 1);
-	assert(debug_memory[200] == 0xffffffdf);
-	assert(x);
+    assert(debug_memory[100] == 1);
+    assert(debug_memory[200] == 0xffffffdf);
+    assert(x);
 }
